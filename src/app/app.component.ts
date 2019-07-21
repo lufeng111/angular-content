ef923c3dfabc962b8c2172f252e904df2eaa60a1
lufeng111 <1836232210@qq.com>
2019-07-21 13:10:36 +0800

    ng-content 投影

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-content';
+
+  // 这个divContent 就是一个html,通过输定绑定绑到模板的div上，这个html展示在父组件上
+  divContent = "<div>慕课网</div>";s
 }
